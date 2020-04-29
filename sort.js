9872951d5b0ae315bccc19e881bd86a23b94eeb1
aaron-plahn <aplahn85@gmail.com>
2020-04-28 22:23:35 -0700

    Optimize for early sort completion

--- sort.js
+++ sort.js
@@ -1,10 +1,15 @@
 'use strict';
 console.log(bubbleSort([4,2,3,6,3,1,9,7]));
 function bubbleSort(a){
+    let noSwaps = true;
     for(let i = a.length; i>0; i--){
         for(let j=0; j < i; j++){
-            if(a[j]>a[j+1]) swapArrayElementsAtIndices(a,j,j+1);
+            if(a[j]>a[j+1]){
+                swapArrayElementsAtIndices(a,j,j+1);
+                noSwaps = false;
+            } 
         }
+        if(noSwaps) break;
     }
     return a;
 }
