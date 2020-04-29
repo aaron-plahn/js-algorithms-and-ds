0b50bf5a9113f010d816511f9024d640d5f63bf1
aaron-plahn <aplahn85@gmail.com>
2020-04-28 22:03:25 -0700

    Implement bubble sort.

---
+++ sort.js
@@ -0,0 +1,18 @@
+'use strict';
+console.log(bubbleSort([4,2,3,6,3,1,9,7]));
+function bubbleSort(a){
+    for(let i = a.length; i>0; i--){
+        for(let j=0; j < i; j++){
+            if(a[j]>a[j+1]) swapArrayElementsAtIndices(a,j,j+1);
+        }
+    }
+    return a;
+}
+
+function swapArrayElementsAtIndices(a,i,j){
+    let length = a.length;
+    if(i >= length || i < 0 || j >= length || j < 0 ) throw new Error(`Index out of bounds`);
+    let temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
