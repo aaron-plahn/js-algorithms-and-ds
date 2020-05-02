d75896dab890f6b75b3c9f54d64e480aa3f55d8b
aaron-plahn <aplahn85@gmail.com>
2020-05-02 10:49:46 -0700

    Implement merge function for sorted arrays.

--- sort.js
+++ sort.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log(insertionSort([4,2,3,6,3,1,9,7]));
+console.log(mergeSortedArrays([1,2,4,7],[1,3,5,9]));
 function bubbleSort(a){
     let noSwaps;
     for(let i = a.length; i>0; i--){
@@ -46,4 +46,31 @@ function swapArrayElementsAtIndices(a,i,j){
     let temp = a[i];
     a[i] = a[j];
     a[j] = temp;
+}
+
+function mergeSortedArrays(a1,a2){
+    let i1 = 0; // 1st array pointer
+    let i2 = 0; //2nd array pointer
+    let mergedArray = [];
+    while(i1 < a1.length && i2 < a2.length){
+        debugger;
+        if(i1 === a1.length){
+            mergedArray.push(a2[i2]);
+            i2++;
+            continue;
+        }
+        if(i2 === a2.length){
+            mergedArray.push(a1[i1]);
+            i1++;
+            continue;
+        }
+        if(a1[i1] < a2[i2]){
+            mergedArray.push(a1[i1]);
+            i1++;
+        }else{
+            mergedArray.push(a2[i2]);
+            i2++;
+        }
+    }
+    return mergedArray;
 }
