bf77bc717a454fd65c723811b043023bc8a45aab
aaron-plahn <aplahn85@gmail.com>
2020-05-03 22:13:33 -0700

    Completed Exercise 18: flatten

--- flatten.js
+++ flatten.js
@@ -1,1 +1,18 @@
 'use strict';
+console.log(flatten([1,2,[3,[4,5],[6,7,8],9],[10,11,12]]));
+// passed
+function flatten(a){
+    // expects a to be an array of arrays to flatten
+    let aFlat = [];
+    function flatHelper(a){
+        for(let i=0; i<a.length; i++){
+            if(Array.isArray(a[i])){
+                flatHelper(a[i]);
+            }else{
+                aFlat.push(a[i]);
+            }
+        }
+    }
+    flatHelper(a);
+    return aFlat;
+}
