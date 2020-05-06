169aebf8592a90ae986565e0ded78dbfced9b52d
aaron-plahn <aplahn85@gmail.com>
2020-05-05 21:15:34 -0700

    Implement singly-linked-list

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -4,3 +4,24 @@ class Node {
         this.next = null;
     }
 }
+
+class singlyLinkedList{
+    constructor(){
+        // create empty list
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(v){
+        let newNode = new Node(v);
+        if(head){
+            this.tail.next = newNode; // set next on former tail
+        } else{
+            this.head = newNode;
+        }
+        this.tail = newNode; // update tail either way
+        this.length++;
+        return this; // return list
+    }
+}
