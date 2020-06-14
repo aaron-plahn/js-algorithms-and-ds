62c7d1b1efc8f2c1c404c66c749cdc8cc59fc179
aaron-plahn <aplahn85@gmail.com>
2020-06-14 13:37:21 -0700

    Implement delete on Trie

--- trie.js
+++ trie.js
@@ -20,7 +20,6 @@ class Trie{
                 current.children[currentChar] = node; // attach child node for given char
             } 
             current = node;
-            debugger;
         }
         current.endOfWord = true; // for loop terminates with current === last character of word
     }
@@ -31,11 +30,32 @@ class Trie{
         for(let i=0; i<word.length; i++){
             let currentChar = word.charAt(i);
             let node = current.children[currentChar];
-            debugger;
             if(!node) return false;
             current = node;
         }
         return current.endOfWord;
+    }
+
+    delete(word){
+        this.deleteRecursively(word,this.root,0);
+    }
+
+    deleteRecursively(word,current,index){
+        debugger;
+        if(index === word.length){
+            if(!current.endOfWord) return false;
+            current.endOfWord = false; // delete word if it was there
+            return Object.keys(current.children).length === 0;
+        }
+        let currentChar = word.charAt(index);
+        let node = current.children[currentChar];
+        if(!node) return false;
+        let shouldDeleteCurrentNode = this.deleteRecursively(word,node,index+1);
+        if(shouldDeleteCurrentNode){
+            delete current.children[currentChar];
+            return Object.keys(current.children).length === 0;
+        }
+        return false;
     }
 }
 
@@ -44,5 +64,9 @@ t.insert("bat");
 t.insert("bob");
 t.insert("ball");
 t.insert("tom");
+console.log(`Trie contains the word bob: ${t.search("bob")}`);
+console.log(`Trie contains the word bobby: ${t.search("bobby")}`);
+t.insert("bobby");
+console.log(`Deleted the word bob: ${t.delete("bob")}`);
 console.log(`Trie contains the word bob: ${t.search("bob")}`);
 console.log(`Trie contains the word bobby: ${t.search("bobby")}`);
